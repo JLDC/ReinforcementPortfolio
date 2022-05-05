9025fce30e2f72080a0114e1ad99ddaff763622f
JLDC <jonathan.chassot@unisg.ch>
2022-05-05 23:26:51 +0200

    Update LQCPolicy to be nonlinear

--- src/policies/optimal/LQCPolicy.jl
+++ src/policies/optimal/LQCPolicy.jl
@@ -11,9 +11,9 @@ end
 function (p::LQCPolicy)(::PreEpisodeStage, env::SimulatorEnv)
     @unpack sim, T, fee, w, f = env
     @unpack B, Φ, Σ, Ψ = sim
-    @unpack verbose, γ = p
+    @unpack verbose = p
     N = nassets(env)
-    Λ = diagm([fee for _ ∈ 1:N])
+    Bf = [exp.(B*f[:, t]) for t ∈ 1:size(f, 2)]
     pf_model = Model(Ipopt.Optimizer)
     verbose || set_optimizer_attribute(pf_model, "print_level", 0)
     # Portfolio weights over time as matrix (no shorting or leverage)
@@ -21,17 +21,19 @@ function (p::LQCPolicy)(::PreEpisodeStage, env::SimulatorEnv)
     # Add constraint on simplex for each period
     @constraint(pf_model, [t ∈ 1:T], sum(x[:, t]) == 1)
     # Cost function
-    @objective(pf_model, Max,
-        x[:, 1]'B*f[:, 1] - .5*(x[:, 1]-w)'Λ*(x[:, 1]-w) +
-        sum((
-            x[:, t]'B*f[:, t+1] 
-            - .5*(x[:, t]-x[:, t-1])'Λ*(x[:, t]-x[:, t-1])) 
-        for t ∈ 2:T)
+    @NLobjective(pf_model, Max,
+        sum(x[i, 1] * Bf[1][i] for i ∈ 1:N) * (1 - fee * sum(abs(x[i, 1] - w[i]) for i ∈ 1:N)) +
+        sum(
+            sum(x[i, t] * Bf[t][i] for i ∈ 1:N) * (1 - fee * sum(abs(x[i, t] - x[i, t-1]) for i ∈ 1:N))
+            for t ∈ 2:T
+        )
     )
     optimize!(pf_model)
     # Update weights with optimal solution
     p.w = value.(x)
 end
+
+
 
 (p::LQCPolicy)(env::SimulatorEnv) = p.w[:, env.t]
 
